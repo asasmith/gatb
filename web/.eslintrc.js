703de07162d49ac4689e3f0b9f62cb314e14fce3
Asa Smith <smith.asa.la@gmail.com>
2019-10-20 21:13:10 -0400

    update eslint rules

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   extends: ['standard', 'standard-react', 'plugin:import/errors', 'plugin:import/warnings'],
   rules: {
     'react/prop-types': 0,
-    'object-curly-spacing': ['error', 'never']
+    'object-curly-spacing': ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
   },
   settings: {
     react: {
